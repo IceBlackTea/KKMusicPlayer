dd92b20c67a78fffee4cc5c62f5ff0a283e533cc
finger <finger@macbook-air.local>
2017-04-07 13:20:59 +0800

    Initial Commit

---
+++ KKMusicPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KKMusicPlayer
+//
+//  Created by finger on 2017/4/7.
+//  Copyright © 2017年 finger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
